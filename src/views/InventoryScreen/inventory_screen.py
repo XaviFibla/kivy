98e6b1d7dc2a124c7a5191643950780d15628553
XaviFibla <xavi.fibla@gmail.com>
2022-11-27 11:48:08 +0100

    Button Camara

--- src/views/InventoryScreen/inventory_screen.py
+++ src/views/InventoryScreen/inventory_screen.py
@@ -3,14 +3,25 @@ import json
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from utils import load_kv
-from kivy.clock import Clock
 from kivymd.uix.list import OneLineListItem
+from kivymd.uix.button import MDFloatingActionButton
 
 load_kv(__name__)
 
 class InventoryScreen(MDScreen):
+ 
     def on_enter(self):
         print("Torno a entrar")
+        self.ids.box.add_widget(
+            MDFloatingActionButton(
+                        icon="qrcode",
+                        type="standard",
+                        theme_icon_color="Custom",
+                        md_bg_color="#fefbff",
+                        icon_color= "#6851a5",
+                        pos_hint= {"center_x": .9, "center_y": .8},
+                        on_release=self.camara
+                    ))
         for i in range(20):
                 self.ids["container"].add_widget(
                 OneLineListItem(text=f"Single-line item {i}",id=str(i),on_press=self.print)
@@ -19,6 +30,10 @@ class InventoryScreen(MDScreen):
 
     def print(self,row):
         print (f"Pressed {row.id}")
+        #app = MDApp.get_running_app()
+        #app.switch_screen('login')
+    def camara(self, *args):
+        print("Obrir Camara")
 
     def list_posts(self):
         self.ids["container"].clear_widgets()
